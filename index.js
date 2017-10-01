d661d48fc7af9811bd2582dea27aab309e2b65b2
Jae Huh <jaeeunhuh@gmail.com>
2017-10-01 16:33:56 +1300

    Add text and images

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const jimp = require('jimp')
 
 jimp.read('./images/sydney-opera-house.jpg').then(img => {
-  img.greyscale()         
-    .write('./images/sydney-opera-house-modified.jpg')
+  img
+    .resize(672, 504)
+    .greyscale()
+    .brightness(0.5)
+  jimp.loadFont(jimp.FONT_SANS_32_WHITE).then(font => {
+    img.print(font, 100, 100, 'Hello Sydney!')
+    jimp.read('./images/cat.jpg').then(cat => {
+      img.composite(cat, 150, 100)
+        .write('./images/sydney-opera-house-modified.jpg')
+    })
+  })
 }).catch(function (err) {
   console.error(err)
 })
-
